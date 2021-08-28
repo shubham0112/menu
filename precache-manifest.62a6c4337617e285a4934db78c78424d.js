0059a4b346c8111e84db512401bb38d7312107b0
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2021-08-28 14:37:27 +0530

    Updates

---
+++ precache-manifest.62a6c4337617e285a4934db78c78424d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88a09b6e2eb29f9e1e1d162b9a7cfb3",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "c8c1d205d59ebda51b6a",
+    "url": "/menu/static/css/main.cedbad78.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c1d205d59ebda51b6a",
+    "url": "/menu/static/js/main.e03ffd2e.chunk.js"
+  },
+  {
+    "revision": "56ef0208991cca3c548d",
+    "url": "/menu/static/js/runtime-main.a5a74f46.js"
+  }
+]);
